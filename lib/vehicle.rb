62ad5b371ba5566bee58d8ee88960bb74780ea04
Milan Mody <milanm1120@gmail.com>
2021-03-23 22:35:39 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,23 @@
 class Vehicle
 
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+
 end
+
+
+v1 = Vehicle.new(20, 45)
+puts v1.go
+puts v1.fill_up_tank
